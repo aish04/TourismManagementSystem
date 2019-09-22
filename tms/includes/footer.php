5f722b660d234b53290132bbc93d3c34428f5c03
aish04 <aish.agarwal97@yahoo.in>
2019-09-22 22:36:45 +0530

    Add files via upload

---
+++ tms/includes/footer.php
@@ -0,0 +1,21 @@
+
+
+
+
+<!--- /footer-top ---->
+<!---copy-right ---->
+<div class="copy-right">
+	<div class="container">
+	
+		<div class="footer-social-icons wow fadeInDown animated animated" data-wow-delay=".5s" style="visibility: visible; animation-delay: 0.5s; animation-name: fadeInDown;">
+			<ul>
+				<li><a class="facebook" href="https://www.facebook.com/login/"><span>Facebook</span></a></li>
+				<li><a class="twitter" href="https://twitter.com/login?lang=en"><span>Twitter</span></a></li>
+				<li><a class="flickr" href="https://www.flickr.com/"><span>Flickr</span></a></li>
+				<li><a class="googleplus" href="https://plus.google.com/discover"><span>Google+</span></a></li>
+				<li><a class="dribbble" href="https://dribbble.com/"><span>Dribbble</span></a></li>
+			</ul>
+		</div>
+		<p class="wow zoomIn animated animated" data-wow-delay=".5s" style="visibility: visible; animation-delay: 0.5s; animation-name: zoomIn;">© 2017 TMS . All Rights Reserved </p>
+	</div>
+</div>
